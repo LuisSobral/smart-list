fcc6613237875618739ccdf6a1c9d067253edf5a
Luis Felipe <sobral93@gmail.com>
2021-04-21 22:59:50 -0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="h-full">
+    <header class="h-10 relative">
+      <nuxt-link to="/">
+        <img class="absolute inset-0 m-auto" src="/images/logo.png" />
+      </nuxt-link>
+    </header>
+    <main class="min-h-screen py-16">
+      <Nuxt />
+    </main>
+    <base-button fab>
+      <img src="/images/add.png" />
+    </base-button>
+    <footer>
+      <div class="container mx-auto py-4 border-t border-gray-300">
+        <p class="text-center">
+          @2021. BLIP Todos os direitos reservados |
+          <span class="font-bold">Termos de Uso</span>
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  overflow-x: hidden;
+}
+</style>
+
+<style lang="scss" scoped>
+header {
+  background-color: $bg-header;
+}
+
+main,
+footer {
+  background-color: #f5f8f9;
+}
+
+footer {
+  color: #939da5;
+}
+</style>
+>
